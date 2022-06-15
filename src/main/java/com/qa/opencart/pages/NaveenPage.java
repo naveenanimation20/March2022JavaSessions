561997a69f0d21d95561a312ed5054218b3dca41
Naveen AutomationLabs <naveenanimation20@gmail.com>
2022-06-15 09:23:31 +0530

    Create NaveenPage.java

---
+++ src/main/java/com/qa/opencart/pages/NaveenPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+public class NaveenPage {
+
+	public static void main(String[] args) {
+
+		
+		int i = 100;
+		System.out.println(i);
+		
+    int j = 200;
+    		System.out.println(j);
+
+		
+	}
+
+}
